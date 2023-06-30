5d1ea1ff2e8489837914ae3184c09528cb281101
Ravichandran Gethushan <85168600+gethushan@users.noreply.github.com>
2023-06-30 07:29:27 +0530

    Update db.config.js
    Your MongoDB url is in your code ...
    Its not a good practice to expose your MongoDB URL to community ..
    So This pull request will delete your mongodb URL from your code ...
    Please Merge This Pull Request...

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // url:"mongodb://localhost:27017/agrix-cluster"
-  url:"mongodb+srv://evanjali:evan123@dashboard-cluster.u3i1c.mongodb.net/schema-data"
+  url:"%%MongoDB-URL%%"
   // url: "mongodb://agrix-api-server:Yo7UvAYcqaN4Y527tWafYoQoClx4KFFg13fCCxXjikYDrUvB6deSe4QMuRpoFChVYAyv0Mt7wB3GjPfRPD5KZw==@agrix-api-server.mongo.cosmos.azure.com:10255/agrix-api-database?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000"
 };
 
