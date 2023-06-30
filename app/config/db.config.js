8cbc2a39445b93458aa128da5920889a5f40935f
Ravichandran Gethushan <85168600+gethushan@users.noreply.github.com>
2023-06-30 07:36:26 +0530

    Update db.config.js
    Detected hardcoded secrets in code 
    
    MongoDB URL

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  // url:"mongodb://localhost:27017/agrix-cluster"
+  // url:"%%MongoDB-URL%%"
   url:"%%MongoDB-URL%%"
-  // url: "mongodb://agrix-api-server:Yo7UvAYcqaN4Y527tWafYoQoClx4KFFg13fCCxXjikYDrUvB6deSe4QMuRpoFChVYAyv0Mt7wB3GjPfRPD5KZw==@agrix-api-server.mongo.cosmos.azure.com:10255/agrix-api-database?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000"
+  // url: "%%MongoDB-URL%%"
 };
 
 
